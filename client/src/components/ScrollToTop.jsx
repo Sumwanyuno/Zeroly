e0298c1a86bafda056d28bf15b4e07258bf3dd70
Sneha <sneha044l2005@gmail.com>
2025-07-26 00:14:00 +0530

    Added contact page

--- client/src/components/ScrollToTop.jsx
+++ client/src/components/ScrollToTop.jsx
@@ -1,27 +1,4 @@
-// // client/src/components/ScrollToTop.jsx
 
-// import { useEffect } from 'react';
-// import { useLocation } from 'react-router-dom';
-
-// const ScrollToTop = () => {
-//   const { pathname, hash } = useLocation();
-
-//   useEffect(() => {
-//     // Check if there's a hash in the URL (meaning it's an anchor link)
-//     // If there's no hash, scroll to top.
-//     // If there is a hash, the HomePage's useLayoutEffect will handle scrolling to the specific ID.
-//     if (!hash) {
-//       window.scrollTo(0, 0); // Scroll to the very top left corner of the window
-//     }
-//     // If there is a hash, and we are on the homepage, HomePage's useLayoutEffect will handle it.
-//     // For other pages with a hash (less common), you might need specific logic.
-
-//   }, [pathname, hash]); // Re-run when pathname or hash changes
-
-//   return null; // This component doesn't render anything
-// };
-
-// export default ScrollToTop;
 // client/src/components/ScrollToTop.jsx
 
 import { useEffect } from 'react';
