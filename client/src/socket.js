0617d17652da648017c99b9caed559f89c0e9b85
Shivam Gupta <shivam93455@gmail.com>
2025-07-26 23:14:10 +0530

    final update

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "https://zeroly-production.up.railway.app";
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 
 console.log("Using SOCKET_URL:", SOCKET_URL);
 
